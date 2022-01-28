5ba2dd244d97cf64f91b0ba40c685ec23fb376e3
David Núñez <david@nucypher.com>
2022-01-28 13:01:00 +0100

    Adapt test to changes in SubscriptionManager API

--- tests/test_subscription_manager.py
+++ tests/test_subscription_manager.py
@@ -6,39 +6,45 @@ from brownie import chain
 
 @pytest.fixture(scope="session")
 def subscription_manager(SubscriptionManager, accounts):
-    return accounts[0].deploy(SubscriptionManager)
+    sm = accounts[0].deploy(SubscriptionManager)
+    sm.initialize(42)
+    return sm
 
 def test_create_policy(subscription_manager, accounts):
     policy_id = b'feed your head!!'
     alice = accounts[1]
     duration = 1000
-    fee = subscription_manager.RATE_PER_SECOND() * duration
+    size = 3
+    fee = subscription_manager.feeRate() * duration * size
     start = chain.time()
     end = start + duration
-    subscription_manager.createPolicy(policy_id, alice, start, end,
+    subscription_manager.createPolicy(policy_id, alice, size, start, end,
         {'from': alice, 'value': fee })
     assert subscription_manager.balance() == fee
 
     assert subscription_manager.isPolicyActive(policy_id)
 
-    policy = subscription_manager.policies(policy_id)
+    policy = subscription_manager.getPolicy(policy_id)
     assert policy[0] == alice
-    assert policy[1] == "0x0000000000000000000000000000000000000000"
-    assert policy[2] == start
-    assert policy[3] == end
+    assert policy[1] == start
+    assert policy[2] == end
+    assert policy[3] == size
+    assert policy[4] == "0x0000000000000000000000000000000000000000"
+    
     
 def test_create_policy_with_same_id(subscription_manager, accounts):
     policy_id = b'feed your head!!'
     alice = accounts[1]
     duration = 1000
-    fee = subscription_manager.RATE_PER_SECOND() * duration
+    size = 3
+    fee = subscription_manager.feeRate() * duration * size
     with brownie.reverts("Policy is currently active"):
-        subscription_manager.createPolicy(policy_id, alice, chain.time(), chain.time() + duration,
+        subscription_manager.createPolicy(policy_id, alice, size, chain.time(), chain.time() + duration,
                                           {'from': alice, 'value': fee })
 
 def test_sweep(subscription_manager, accounts):
     recipient = accounts[2]
     initial_balance = recipient.balance()
-    expected_balance = initial_balance + subscription_manager.RATE_PER_SECOND() * 1000
+    expected_balance = initial_balance + subscription_manager.feeRate() * 1000 * 3
     subscription_manager.sweep(recipient, {'from': accounts[0] })
     assert recipient.balance() == expected_balance
