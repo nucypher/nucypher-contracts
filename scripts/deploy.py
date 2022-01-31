27ccc18053c71cb495017adc2a1256f298016a06
David Núñez <david@nucypher.com>
2022-01-31 10:22:57 +0100

    Use TransparentUpgradeableProxy for SubscriptionManager tests and deployment scripts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 
-from brownie import SubscriptionManager, accounts, Wei
+import brownie
+from brownie import SubscriptionManager, accounts, Contract, Wei
+from brownie.project.main import Project
+
+INITIAL_FEE_RATE = Wei("1 gwei")
 
 def main():
     deployer = accounts.load('test')
-    subscription_manager = SubscriptionManager.deploy({'from':deployer})
-    subscription_manager.initialize(Wei("1 gwei"))
+
+    brownie_packages = brownie._config._get_data_folder().joinpath('packages')
+    oz_dependency_path = 'OpenZeppelin/openzeppelin-contracts@4.4.2/'
+    oz = Project("OZ", brownie_packages.joinpath(oz_dependency_path))
+    
+    proxy_admin = deployer.deploy(oz.ProxyAdmin)
+
+    subscription_manager_logic = deployer.deploy(SubscriptionManager)
+    calldata = subscription_manager_logic.initialize.encode_input(INITIAL_FEE_RATE)
+    transparent_proxy = oz.TransparentUpgradeableProxy.deploy(
+        subscription_manager_logic.address,
+        proxy_admin.address,
+        calldata,
+        {'from': deployer})
+
+    subscription_manager = Contract.from_abi("SubscriptionManager", transparent_proxy.address, subscription_manager_logic.abi, owner=None)
     return subscription_manager
--- tests/test_subscription_manager.py
+++ tests/test_subscription_manager.py
@@ -2,13 +2,48 @@ import pytest
 
 import brownie
 
-from brownie import chain
+from brownie import Contract, chain
+from brownie.convert.datatypes import Wei
+from brownie.project.main import Project
+
+from pathlib import Path
+
+INITIAL_FEE_RATE = Wei("1 gwei")
 
 @pytest.fixture(scope="session")
-def subscription_manager(SubscriptionManager, accounts):
-    sm = accounts[0].deploy(SubscriptionManager)
-    sm.initialize(42)
+def oz():
+    brownie_packages = brownie._config._get_data_folder().joinpath('packages')
+    oz_dependency_path = 'OpenZeppelin/openzeppelin-contracts@4.4.2/'
+    project = Project("OZ", brownie_packages.joinpath(oz_dependency_path))
+    return project
+
+@pytest.fixture(scope="session")
+def proxy_admin(oz, accounts):
+    return accounts[0].deploy(oz.ProxyAdmin)
+
+@pytest.fixture(scope="session")
+def subscription_manager_logic(SubscriptionManager, accounts):
+    return accounts[0].deploy(SubscriptionManager)
+
+@pytest.fixture(scope="session")
+def transparent_proxy(oz,
+                      proxy_admin, subscription_manager_logic, accounts):
+
+    calldata = subscription_manager_logic.initialize.encode_input(INITIAL_FEE_RATE)
+    return oz.TransparentUpgradeableProxy.deploy(
+        subscription_manager_logic.address,
+        proxy_admin.address,
+        calldata,
+        {'from': accounts[0]})
+
+@pytest.fixture(scope="session")
+def subscription_manager(transparent_proxy, subscription_manager_logic):
+    sm = Contract.from_abi("SubscriptionManager", transparent_proxy.address, subscription_manager_logic.abi, owner=None)
     return sm
+
+def test_initial_parameters(subscription_manager, subscription_manager_logic):
+    assert subscription_manager.feeRate() == INITIAL_FEE_RATE
+    assert subscription_manager_logic.feeRate() == 0
 
 def test_create_policy(subscription_manager, accounts):
     policy_id = b'feed your head!!'
@@ -30,7 +65,6 @@ def test_create_policy(subscription_manager, accounts):
     assert policy[2] == end
     assert policy[3] == size
     assert policy[4] == "0x0000000000000000000000000000000000000000"
-    
     
 def test_create_policy_with_same_id(subscription_manager, accounts):
     policy_id = b'feed your head!!'
