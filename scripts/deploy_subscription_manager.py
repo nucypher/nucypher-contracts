215452a36ce0e2935d527e2f7562cf9cb8dd2383
David Núñez <david@nucypher.com>
2022-02-17 10:42:50 +0100

    Workaround to save deployment data of dependency contracts

--- scripts/deploy_subscription_manager.py
+++ scripts/deploy_subscription_manager.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 import brownie
-from brownie import SubscriptionManager, accounts, Contract, Wei
+from brownie import SubscriptionManager, accounts, project, Contract, Wei
+from brownie.project import compiler
 from brownie.project.main import Project
+
+from pathlib import Path
 
 INITIAL_FEE_RATE = Wei("1 gwei")
 
@@ -10,10 +13,13 @@ def main():
     deployer = accounts.load('test')
 
     # TODO: There has to be a better way to do this resolution. See #13
+    current_project = project.get_loaded_projects().pop()
     brownie_packages = brownie._config._get_data_folder().joinpath('packages')
     oz_dependency_path = 'OpenZeppelin/openzeppelin-contracts@4.5.0/'
     oz = Project("OZ", brownie_packages.joinpath(oz_dependency_path))
-    
+    # FIXME: This is a workaround to save deployment data in the same project
+    oz._build_path = current_project._build_path
+
     proxy_admin = deployer.deploy(oz.ProxyAdmin)
 
     subscription_manager_logic = deployer.deploy(SubscriptionManager)
