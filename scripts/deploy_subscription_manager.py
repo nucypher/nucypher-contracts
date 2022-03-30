d3a121f7bb6d131dd498585913174d63bda73587
James Campbell <james.campbell@tanti.org.uk>
2022-03-30 14:22:12 -0500

    Load OpenZeppelin contracts directly

--- scripts/deploy_subscription_manager.py
+++ scripts/deploy_subscription_manager.py
@@ -11,27 +11,19 @@ from brownie.project.main import Project
 INITIAL_FEE_RATE = Wei("1 gwei")
 
 
-def main(account_name : str = 'test'):
+def main(account_name: str = "test"):
     deployer = accounts.load(account_name)
-
-    # TODO: There has to be a better way to do this resolution. See #13
-    current_project = project.get_loaded_projects().pop()
-    brownie_packages = brownie._config._get_data_folder().joinpath('packages')
-    oz_dependency_path = 'OpenZeppelin/openzeppelin-contracts@4.5.0/'
-    oz = Project("OZ", brownie_packages.joinpath(oz_dependency_path))
-    # FIXME: This is a workaround to save deployment data in the same project
-    oz._build_path = current_project._build_path
+    oz = project.load("OpenZeppelin/openzeppelin-contracts@4.5.0/")
 
     proxy_admin = deployer.deploy(oz.ProxyAdmin)
 
     subscription_manager_logic = deployer.deploy(SubscriptionManager)
     calldata = subscription_manager_logic.initialize.encode_input(INITIAL_FEE_RATE)
     transparent_proxy = oz.TransparentUpgradeableProxy.deploy(
-        subscription_manager_logic.address,
-        proxy_admin.address,
-        calldata,
-        {'from': deployer})
+        subscription_manager_logic.address, proxy_admin.address, calldata, {"from": deployer}
+    )
 
     subscription_manager = Contract.from_abi(
-        "SubscriptionManager", transparent_proxy.address, subscription_manager_logic.abi, owner=None)
+        "SubscriptionManager", transparent_proxy.address, subscription_manager_logic.abi, owner=None
+    )
     return subscription_manager
