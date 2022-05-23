75893a4d26bf9f8501e71992f0a1544fa18f92f3
James Campbell <james.campbell@tanti.org.uk>
2022-05-23 15:23:30 +0300

    Refactor deploying mocks into `unit` script

--- scripts/deploy_staking_escrow.py
+++ scripts/deploy_staking_escrow.py
@@ -1,29 +1,13 @@
 #!/usr/bin/python3
-from brownie import (
-    NuCypherToken,
-    StakingEscrow,
-    ThresholdStakingForStakingEscrowMock,
-    WorkLockForStakingEscrowMock,
-    config,
-)
-
-from scripts.utils import CURRENT_NETWORK, LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
-
-
-def deploy_mocks(deployer):
-    """This function should deploy nucypher_token and t_staking and return the
-    corresponding contract addresses"""
-    nucypher_token = NuCypherToken.deploy(1_000_000_000, {"from": deployer})
-    t_staking = ThresholdStakingForStakingEscrowMock.deploy({"from": deployer})
-    work_lock = WorkLockForStakingEscrowMock.deploy(nucypher_token, {"from": deployer})
-    return nucypher_token, t_staking, work_lock
+from brownie import StakingEscrow, config
+from scripts.utils import CURRENT_NETWORK, LOCAL_BLOCKCHAIN_ENVIRONMENTS, deploy_mocks, get_account
 
 
 def main(account_id=None):
     deployer = get_account(account_id)
     network_config = config["networks"][CURRENT_NETWORK]
     if CURRENT_NETWORK in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        nucypher_token, t_staking, work_lock = deploy_mocks(deployer)
+        nucypher_token, t_staking, work_lock, _ = deploy_mocks(deployer)
     else:
         nucypher_token = network_config.get("nu_token")
         t_staking = network_config.get("t_staking")
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,24 @@
-from brownie import accounts, network
+from brownie import (
+    NuCypherToken,
+    StakingEscrow,
+    ThresholdStakingForStakingEscrowMock,
+    WorkLockForStakingEscrowMock,
+    accounts,
+    network,
+)
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development"]
 PRODUCTION_ENVIRONMENTS = ["mainnet", "polygon-main"]
 CURRENT_NETWORK = network.show_active()
+
+def deploy_mocks(deployer):
+    """This function should deploy nucypher_token and t_staking and return the
+    corresponding contract addresses"""
+    nucypher_token = NuCypherToken.deploy(1_000_000_000, {"from": deployer})
+    t_staking = ThresholdStakingForStakingEscrowMock.deploy({"from": deployer})
+    work_lock = WorkLockForStakingEscrowMock.deploy(nucypher_token, {"from": deployer})
+    staking_escrow = StakingEscrow.deploy(nucypher_token, 0, 100_000, {"from": deployer})
+    return nucypher_token, t_staking, work_lock, staking_escrow
 
 
 def get_account(id):
