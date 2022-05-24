10a2d485a08f767adbb50f73013c81fba69d99b0
James Campbell <james.campbell@tanti.org.uk>
2022-05-24 17:17:11 +0300

    Create deploy script for Simple PRE

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,7 @@ from brownie import (
     NuCypherToken,
     StakingEscrow,
     ThresholdStakingForStakingEscrowMock,
+    ThresholdStakingForPREApplicationMock,
     WorkLockForStakingEscrowMock,
     accounts,
     network,
@@ -11,14 +12,18 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development"]
 PRODUCTION_ENVIRONMENTS = ["mainnet", "polygon-main"]
 CURRENT_NETWORK = network.show_active()
 
+
 def deploy_mocks(deployer):
     """This function should deploy nucypher_token and t_staking and return the
     corresponding contract addresses"""
     nucypher_token = NuCypherToken.deploy(1_000_000_000, {"from": deployer})
-    t_staking = ThresholdStakingForStakingEscrowMock.deploy({"from": deployer})
+    t_staking_for_escrow = ThresholdStakingForStakingEscrowMock.deploy({"from": deployer})
+    t_staking_for_pre = ThresholdStakingForPREApplicationMock.deploy({"from": deployer})
     work_lock = WorkLockForStakingEscrowMock.deploy(nucypher_token, {"from": deployer})
-    staking_escrow = StakingEscrow.deploy(nucypher_token, 0, 100_000, {"from": deployer})
-    return nucypher_token, t_staking, work_lock, staking_escrow
+    staking_escrow = StakingEscrow.deploy(
+        nucypher_token, work_lock, t_staking_for_escrow, {"from": deployer}
+    )
+    return nucypher_token, t_staking_for_escrow, t_staking_for_pre, work_lock, staking_escrow
 
 
 def get_account(id):
