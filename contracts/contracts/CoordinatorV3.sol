baacbcd529cace1451dbaab84c0acb4beebea384
derekpierre <derek.pierre@gmail.com>
2023-02-10 10:28:42 -0500

    Various updates to coordinator: - Store dkg size and restrict via a max dkg size - Store transcripts as bytes (so can be reused for aggregated transcripts) - Add detail to failed status - Improve efficiency of finalizeRitual to compare all transcripts to the first one - Only store keccack digests of transcripts - Ritual struct stores finalized public material - Introduces WAITING_FOR_FINALIZATION state - Removes node sorting requirement - EndRitual event inludes initiator address for signaling

--- contracts/contracts/CoordinatorV3.sol
+++ contracts/contracts/CoordinatorV3.sol
@@ -9,9 +9,7 @@ import "./proxy/Upgradeable.sol";
 * @notice Coordination layer for DKG-TDec
 */
 contract CoordinatorV3 is Upgradeable {
-
-    uint256 DKG_SIZE = 8;
-
+    
     // Ritual
     event StartRitual(uint32 indexed ritualId, address[] nodes, address initiator);
     event StartTranscriptRound(uint32 indexed ritualId);
@@ -20,63 +18,69 @@ contract CoordinatorV3 is Upgradeable {
 
     // Node
     event TranscriptPosted(uint32 indexed ritualId, address indexed node, bytes32 transcriptDigest);
-    event AggregationPosted(uint32 indexed ritualId, address indexed node, address[] confirmedNodes);
+    event AggregationPosted(uint32 indexed ritualId, address indexed node, bytes32 aggregatedTranscriptDigest);
 
     // Admin
     event TimeoutChanged(uint32 oldTimeout, uint32 newTimeout);
-   // event DkgSizeChanged(uint32 oldSize, uint32 newSize);
+    event MaxDkgSizeChanged(uint32 oldSize, uint32 newSize);
 
     enum RitualStatus {
         WAITING_FOR_TRANSCRIPTS,
-        WAITING_FOR_CONFIRMATIONS,
-        COMPLETED,
-        FAILED,
-        FINAL
+        WAITING_FOR_AGGREGATIONS,
+        WAITING_FOR_FINALIZATION,
+        FAILED_TIMEOUT,
+        FAILED_INVALID_TRANSCRIPTS,
+        FINALIZED
     }
 
     struct Rite {
         address node;
         bool aggregated;
-        bytes32 transcript;
+        bytes transcript;
     }
 
     struct Ritual {
         uint32 id;
         address initiator;
+        uint32 dkgSize;
         uint32 initTimestamp;
         uint32 totalTranscripts;
-        uint32 totalConfirmations;
+        uint32 totalAggregations;
         uint32 threshold;
+        bytes32 publicMaterial;
         RitualStatus status;
-        Rite[DKG_SIZE] rite;
+        Rite[] rite;
     }
 
     Ritual[] public rituals;
 
+    uint32 public timeout;
+    uint32 public maxDkgSize;
+
     constructor(uint32 _timeout) {
         timeout = _timeout;
+        maxDkgSize = 64;  // TODO Who knows? https://www.youtube.com/watch?v=hzqFmXZ8tOE&ab_channel=Protoje
     }
 
-    function checkActiveRitual(Ritual ritual) {
-        delta = uint32(block.timestamp) - ritual.initTimestamp;
-        if (delta > TIMEOUT) {
-            ritual.status = RitualStatus.FAILED;
-            emit Timeout(ritualId);  // penalty hook, missing nodes can be known at this stage
-            emit EndRitual(ritualId, ritual.status);
+    function _checkActiveRitual(Ritual storage _ritual) internal {
+        uint32 delta = uint32(block.timestamp) - _ritual.initTimestamp;
+        if (delta > timeout) {
+            _ritual.status = RitualStatus.FAILED_TIMEOUT;
+            emit EndRitual(_ritual.id, _ritual.status); // penalty hook, missing nodes can be known at this stage
             revert("Ritual timed out");
         }
     }
 
     function setTimeout(uint32 newTimeout) external onlyOwner {
-        oldTimeout = timeout;
+        uint32 oldTimeout = timeout;
         timeout = newTimeout;
-        emit TimeoutChanged(oldTimeout, NewTimeout);
+        emit TimeoutChanged(oldTimeout, newTimeout);
     }
 
-    function setDkgSize(uint32 newSize) external onlyOwner {
-        oldSize = dkgSize;
-        DKG_SIZE = newSize;
-        emit DkgSizeChanged(oldSize, newSize);
+    function setMaxDkgSize(uint32 newSize) external onlyOwner {
+        uint32 oldSize = maxDkgSize;
+        maxDkgSize = newSize;
+        emit MaxDkgSizeChanged(oldSize, newSize);
     }
 
     function numberOfRituals() external view returns(uint256) {
@@ -92,16 +96,14 @@ contract CoordinatorV3 is Upgradeable {
     }
 
     function initiateRitual(address[] calldata nodes) external returns (uint32) {
-        // TODO: Check for payment
-        // TODO: Check for expiration time
-        // TODO: Improve DKG size choices
-        require(nodes.length == DKG_SIZE, "Invalid number of nodes");
+        require(nodes.length <= maxDkgSize, "Invalid number of nodes");
 
         uint32 id = uint32(rituals.length);
         Ritual storage ritual = rituals.push();
         ritual.id = id;
         ritual.initiator = msg.sender;
         ritual.threshold = threshold;
+        ritual.dkgSize = uint32(nodes.length);
         ritual.initTimestamp = uint32(block.timestamp);
         ritual.status = RitualStatus.WAITING_FOR_TRANSCRIPTS;
 
@@ -110,7 +112,6 @@ contract CoordinatorV3 is Upgradeable {
         address currentNode;
         for(uint256 i=1; i < nodes.length; i++){
             currentNode = nodes[i];
-            require(currentNode > previousNode, "Nodes must be sorted");
             ritual.rite[i].node = currentNode;
             previousNode = currentNode;
             // TODO: Check nodes are eligible (staking, etc)
@@ -123,63 +124,61 @@ contract CoordinatorV3 is Upgradeable {
     function postTranscript(uint32 ritualId, uint256 nodeIndex, bytes calldata transcript) external {
         Ritual storage ritual = rituals[ritualId];
         require(ritual.rite[nodeIndex].node == msg.sender, "Node not part of ritual");
-
         require(ritual.status == RitualStatus.WAITING_FOR_TRANSCRIPTS, "Not waiting for transcripts");
-        require(ritual.rite[nodeIndex].transcript == bytes32(0), "Node already posted transcript");
+        require(ritual.rite[nodeIndex].transcript.length == 0, "Node already posted transcript");
         require(ritual.rite[nodeIndex].aggregated == false, "Node already posted aggregation");
-
-        checkActiveRitual(ritualId);
+        _checkActiveRitual(ritual);
 
         // Nodes commit to their transcript
         bytes32 transcriptDigest = keccak256(transcript);
-        ritual.rite[nodeIndex].transcript = transcriptDigest;
+        ritual.rite[nodeIndex].transcript = transcript;
         emit TranscriptPosted(ritualId, msg.sender, transcriptDigest);
         ritual.totalTranscripts++;
 
-        if (ritual.totalTranscripts == DKG_SIZE){
-            ritual.status = RitualStatus.WAITING_FOR_CONFIRMATIONS;
+        // end round
+        if (ritual.totalTranscripts == ritual.dkgSize){
+            ritual.status = RitualStatus.WAITING_FOR_AGGREGATIONS;
             emit StartAggregationRound(ritualId);
         }
     }
 
-    function postConfirmation(uint32 ritualId, uint256 nodeIndex, bytes calldata aggregatedTranscripts) external {
+    function postAggregation(uint32 ritualId, uint256 nodeIndex, bytes calldata aggregatedTranscripts) external {
         Ritual storage ritual = rituals[ritualId];
-        require(ritual.status == RitualStatus.WAITING_FOR_CONFIRMATIONS, "Not waiting for confirmations");
+        require(ritual.status == RitualStatus.WAITING_FOR_AGGREGATIONS, "Not waiting for confirmations");
         require(ritual.rite[nodeIndex].node == msg.sender, "Node not part of ritual");
-        checkActiveRitual(ritualId);
+        _checkActiveRitual(ritual);
 
         // nodes commit to their aggregation result
         bytes32 aggregatedTranscriptDigest = keccak256(aggregatedTranscripts);
         ritual.rite[nodeIndex].transcript = aggregatedTranscriptDigest;
         ritual.rite[nodeIndex].aggregated = true;
-        ritual.totalConfirmations++;
-
-        if (ritual.totalConfirmations == DKG_SIZE){
-            ritual.status = RitualStatus.COMPLETED;
-            emit EndRitual(ritualId, ritual.status);
-        }
         emit AggregationPosted(ritualId, msg.sender, aggregatedTranscripts);
+        ritual.totalAggregations++;
+
+        // end round
+        if (ritual.totalAggregations == ritual.dkgSize){
+            ritual.status = RitualStatus.WAITING_FOR_FINALIZATION;
+            emit EndRitual(ritualId, ritual.status, ritual.initiator);
+        }
     }
 
-    function finalizeRitual(uint32 ritualId) {
+    function finalizeRitual(uint32 ritualId) public {
         Ritual storage ritual = rituals[ritualId];
-        require(ritual.status == RitualStatus.COMPLETED, 'ritual not complete');
+        require(ritual.status == RitualStatus.WAITING_FOR_FINALIZATION, 'ritual cannot be finalized');
 
-        prev_rite = ritual.rite[0];
+        bytes32 firstRiteDigest = keccak256(ritual.rite[0].transcript);
         for(uint32 i=1; i < ritual.rite.length; i++){
-            rite = ritual.rite[i];
-            if (rite.transcript != prev_rite.transcript) {
-                ritual.status = RitualStatus.FAILED;
-                emit EndRitual(ritualId, ritual.status);
+            bytes32 currentRiteDigest = keccak256(ritual.rite[i].transcript);
+            if (firstRiteDigest != currentRiteDigest) {
+                ritual.status = RitualStatus.FAILED_INVALID_TRANSCRIPTS;
+                emit EndRitual(ritualId, ritual.status, ritual.initiator);
                 revert('aggregated transcripts do not match');
             }
-            prev_rite = rite;
         }
-
-        // mark as finalized
-        ritual.status = RitualStatus.FINAL;
+        
+        ritual.publicMaterial = firstRiteDigest;
+        ritual.status = RitualStatus.FINALIZED;
         emit EndRitual(ritualId, ritual.status, ritual.initiator);
     }
-
 
 }
