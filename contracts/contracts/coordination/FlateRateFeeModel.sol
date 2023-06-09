b8492bf18d3280e6fa1b7ffcf1a59bb4636f31c5
David Núñez <david@nucypher.com>
2023-06-09 08:21:26 +0200

    Basic FlateRateFeeModel

---
+++ contracts/contracts/coordination/FlateRateFeeModel.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "./IFeeModel.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+* @title FlatRateFeeModel
+* @notice FlateRateFeeModel
+*/
+contract FlatRateFeeModel is IFeeModel {
+
+    IERC20 public immutable currency;
+    uint256 public immutable feeRatePerSecond;
+
+    constructor(IERC20 _currency, uint256 _feeRatePerSecond){
+        currency = _currency;
+        feeRatePerSecond = _feeRatePerSecond;
+    }
+
+    function getRitualInitiationCost(
+        address[] calldata providers,
+        uint32 duration
+    ) external view returns(uint256){
+        uint256 size = providers.length;
+        require(duration > 0, "Invalid ritual duration");
+        require(size > 0, "Invalid ritual size");
+        return feeRatePerSecond * size * duration;
+    }
+}
