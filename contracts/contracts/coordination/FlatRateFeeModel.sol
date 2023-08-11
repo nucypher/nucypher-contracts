c4848aea3709f0d8efe60cb6086ab900220a1816
David Núñez <david@nucypher.com>
2023-08-11 12:28:26 +0200

    It's FlatRate, not FlateRate....

--- contracts/contracts/coordination/FlatRateFeeModel.sol
+++ contracts/contracts/coordination/FlatRateFeeModel.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title FlatRateFeeModel
- * @notice FlateRateFeeModel
+ * @notice FlatRateFeeModel
  */
 contract FlatRateFeeModel is IFeeModel {
     IERC20 public immutable currency;
