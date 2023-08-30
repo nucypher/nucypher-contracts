7b7899c20131db1fb5e0e3ec2df8db82befdcc7d
Victoria Zotova <vikki.zta@gmail.com>
2023-08-30 09:29:55 -0400

    Update contracts for coordinator test

---
+++ contracts/test/TACoChildApplicationTestSet.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "../contracts/coordination/ITACoRootToChild.sol";
+import "../contracts/coordination/ITACoChildToRoot.sol";
+
+/**
+ * @notice Contract for testing TACo child application contract
+ */
+contract RootApplicationForTACoChildApplicationMock {
+    ITACoRootToChild public childApplication;
+
+    mapping(address => bool) public confirmations;
+
+    function setChildApplication(ITACoRootToChild _childApplication) external {
+        childApplication = _childApplication;
+    }
+
+    function updateOperator(address _stakingProvider, address _operator) external {
+        childApplication.updateOperator(_stakingProvider, _operator);
+    }
+
+    function updateAuthorization(address _stakingProvider, uint96 _amount) external {
+        childApplication.updateAuthorization(_stakingProvider, _amount);
+    }
+
+    function confirmOperatorAddress(address _operator) external {
+        confirmations[_operator] = true;
+    }
+
+    function resetConfirmation(address _operator) external {
+        confirmations[_operator] = false;
+    }
+}
+
+contract CoordinatorForTACoChildApplicationMock {
+    ITACoChildToRoot public immutable application;
+
+    constructor(ITACoChildToRoot _application) {
+        application = _application;
+    }
+
+    function confirmOperatorAddress(address _operator) external {
+        application.confirmOperatorAddress(_operator);
+    }
+}
