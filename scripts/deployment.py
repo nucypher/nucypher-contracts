8399b1d4a50b924260d0cddb99256d20f0757079
Kieran Prasch <kieranprasch@gmail.com>
2023-09-21 16:59:39 +0200

    Code and style optimization; Use pure helper functions that do not require state, ensure named return values, more type hints, and inline commentary.

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -15,30 +15,49 @@ VARIABLE_PREFIX = "$"
 def prepare_deployment(
     params_filepath: Path, publish: bool
 ) -> typing.Tuple[AccountAPI, "ApeDeploymentParameters"]:
-    check_etherscan_plugin()
-    deployer = get_user_selected_account()
 
+    # pre-deployment checks
+    check_etherscan_plugin()
+    deployer_account = get_user_selected_account()
+
+    # load deployment parameters
     constructor_parameters = ConstructorParameters.from_file(params_filepath)
     deployment_parameters = ApeDeploymentParameters(constructor_parameters, publish)
 
-    return deployer, deployment_parameters
+    return deployer_account, deployment_parameters
 
 
 def _is_variable(param: Any) -> bool:
-    return isinstance(param, str) and param.startswith(VARIABLE_PREFIX)
+    result = isinstance(param, str) and param.startswith(VARIABLE_PREFIX)
+    return result
 
 
 def _resolve_param(value: Any, context: typing.Dict[str, Any]) -> Any:
     if not _is_variable(value):
-        return value
+        return value  # literally a value
     variable = value.strip(VARIABLE_PREFIX)
     contract_instance = context[variable]
     return contract_instance.address
 
 
+def _resolve_list(value: List[Any], context: typing.Dict[str, Any]) -> List[Any]:
+    params = [_resolve_param(v, context) for v in value]
+    return params
+
+
+def _resolve_parameters(parameters: OrderedDict, context: typing.Dict[str, Any]) -> OrderedDict:
+    resolved_params = OrderedDict()
+    for name, value in parameters.items():
+        if isinstance(value, list):
+            resolved_params[name] = _resolve_list(value, context)
+        else:
+            resolved_params[name] = _resolve_param(value, context)
+    return resolved_params
+
+
 def _validate_constructor_param(param: Any, contracts: List[str]) -> None:
     if not _is_variable(param):
-        return
+        return  # literally a value
     variable = param.strip(VARIABLE_PREFIX)
     if variable not in contracts:
         raise ConstructorParameters.Invalid(f"Variable {param} is not resolvable")
@@ -55,19 +74,23 @@ def validate_constructor_parameters(config: typing.OrderedDict[str, Any]) -> Non
                 _validate_constructor_param(value, available_contracts)
 
 
+def _confirm_deployment(contract_name: str) -> None:
+    answer = input(f"Deploy {contract_name} Y/N? ")
+    if answer.lower().strip() == "n":
+        print("Aborting deployment!")
+        exit(-1)
+
+
 def _confirm_resolution(resolved_params: OrderedDict, contract_name: str) -> None:
     if len(resolved_params) == 0:
-        # Nothing really to confirm
-        print(f"(i) No constructor parameters for {contract_name}; proceeding")
+        print(f"(i) No constructor parameters for {contract_name}")
+        _confirm_deployment(contract_name)
         return
 
     print(f"Constructor parameters for {contract_name}")
     for name, resolved_value in resolved_params.items():
         print(f"\t{name}={resolved_value}")
-    answer = input("Deploy Y/N? ")
-    if answer.lower().strip() == "n":
-        print("Aborting deployment!")
-        exit(-1)
+    _confirm_deployment(contract_name)
 
 
 class ConstructorParameters:
@@ -79,23 +102,14 @@ class ConstructorParameters:
         self.parameters = parameters
 
     @classmethod
-    def from_file(cls, params_filepath: Path) -> "ConstructorParameters":
-        with open(params_filepath, "r") as params_file:
+    def from_file(cls, filepath: Path) -> "ConstructorParameters":
+        with open(filepath, "r") as params_file:
             config = OrderedDict(json.load(params_file))
         return cls(config)
 
     def resolve(self, contract_name: str, context: typing.Dict[str, Any]) -> OrderedDict:
-        resolved_params = OrderedDict()
-        for name, value in self.parameters[contract_name].items():
-            if isinstance(value, list):
-                param_value_list = list()
-                for item in value:
-                    param = _resolve_param(item, context)
-                    param_value_list.append(param)
-                resolved_params[name] = param_value_list
-            else:
-                resolved_params[name] = _resolve_param(value, context)
-        return resolved_params
+        result = _resolve_parameters(self.parameters[contract_name], context)
+        return result
 
 
 class ApeDeploymentParameters:
@@ -103,18 +117,17 @@ class ApeDeploymentParameters:
         self.constructor_parameters = constructor_parameters
         self.publish = publish
 
-    def get_kwargs(self):
+    def get_kwargs(self) -> typing.Dict[str, Any]:
         return {"publish": self.publish}
 
-    def get(self, *args, **kwargs):
+    def get(self, *args, **kwargs) -> List[Any]:
         return self.__resolve_deployment_parameters(*args, **kwargs)
 
     def __resolve_deployment_parameters(
-        self, container: ContractContainer, context: typing.Dict[str, Any], interactive: bool = True
+        self, container: ContractContainer, context: typing.Dict[str, Any]
     ) -> List[Any]:
         contract_name = container.contract_type.name
-        resolved_params = self.constructor_parameters.resolve(contract_name, context)
-        if interactive:
-            _confirm_resolution(resolved_params, contract_name)
-
-        return [container, *resolved_params.values()]
+        resolved_constructor_params = self.constructor_parameters.resolve(contract_name, context)
+        _confirm_resolution(resolved_constructor_params, contract_name)
+        deployment_params = [container, *resolved_constructor_params.values()]
+        return deployment_params
