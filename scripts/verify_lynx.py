67f67da22f551cac9ed36ee93845e3f3bf609621
Kieran Prasch <kieranprasch@gmail.com>
2023-09-25 23:15:20 +0200

    modifies standalone verification script to consume a registry

--- scripts/verify_lynx.py
+++ scripts/verify_lynx.py
@@ -1,12 +1,20 @@
+import json
+
 from ape import networks, project
 
-deployments = [
-    project.LynxRootApplication.at('0xe9e6C183fadD057b8972E12471B212288b1ca6E6'),
-    project.LynxTACoChildApplication.at('0x099a6128710F7e30Ed8740644F40EE6d3e6673D1'),
-    project.LynxRitualToken.at('0x871EbA00295fF9c329dbCF2Cd329cd89FD926192'),
-    project.Coordinator.at('0x7c8EA2d03fA65088fA85B889da365035555e4394'),
-    project.GlobalAllowList.at('0xa7aF704855EA2a2513C56212D45b86287205520E'),
-]
+from scripts.constants import ARTIFACTS_DIR
+
+REGISTRY_FILEPATH = ARTIFACTS_DIR / "lynx" / "lynx-alpha-13-child-registry.json"
+
+with open(REGISTRY_FILEPATH) as f:
+    registry = json.loads(f.read())
+
+deployments = []
+for entry in registry:
+    contract_type = entry[0]
+    contract_container = getattr(project, contract_type)
+    contract_instance = contract_container.at(entry[2])
+    deployments.append(contract_instance)
 
 etherscan = networks.provider.network.explorer
 for deployment in deployments:
