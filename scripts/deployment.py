d3c43e1e6c3b05a490dc51c09e35013981d76a6f
derekpierre <derek.pierre@gmail.com>
2023-09-25 15:19:58 -0400

    Add confirmation prompt whenever null address is a deployment parameter.

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -189,6 +189,13 @@ def _confirm_deployment(contract_name: str) -> None:
         exit(-1)
 
 
+def _confirm_null_address() -> None:
+    answer = input(f"Null Address detected for deployment parmaeter; Continue? Y/N? ")
+    if answer.lower().strip() == "n":
+        print("Aborting deployment!")
+        exit(-1)
+
+
 def _confirm_resolution(resolved_params: OrderedDict, contract_name: str) -> None:
     """Asks the user to confirm the resolved constructor parameters for a single contract."""
     if len(resolved_params) == 0:
@@ -197,9 +204,14 @@ def _confirm_resolution(resolved_params: OrderedDict, contract_name: str) -> Non
         return
 
     print(f"\nConstructor parameters for {contract_name}")
+    contains_null_address = False
     for name, resolved_value in resolved_params.items():
         print(f"\t{name}={resolved_value}")
+        if not contains_null_address:
+            contains_null_address = resolved_value == NULL_ADDRESS
     _confirm_deployment(contract_name)
+    if contains_null_address:
+        _confirm_null_address()
 
 
 class ConstructorParameters:
