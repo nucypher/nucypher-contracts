77eb11950bc59a5c54b39933e2e4bd61a147e37c
derekpierre <derek.pierre@gmail.com>
2023-09-26 08:44:00 -0400

    Update merge_registries to use main method.

--- scripts/lynx/merge_registries.py
+++ scripts/lynx/merge_registries.py
@@ -8,9 +8,11 @@ lynx_registry_w_subscription_manager = ARTIFACTS_DIR / "contract_registry.json"
 
 output_registry = ARTIFACTS_DIR / "lynx-alpha-13-merged-registry.json"
 
-merge_registries(
-    registry_1_filepath=lynx_child_deployment_registry,
-    registry_2_filepath=lynx_registry_w_subscription_manager,
-    output_filepath=output_registry,
-    deprecated_contracts=["StakeInfo"],
-)
+
+def main():
+    merge_registries(
+        registry_1_filepath=lynx_child_deployment_registry,
+        registry_2_filepath=lynx_registry_w_subscription_manager,
+        output_filepath=output_registry,
+        deprecated_contracts=["StakeInfo"],
+    )
