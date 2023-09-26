7b576a0978584a81165cf82b1b7a8f9540180de1
derekpierre <derek.pierre@gmail.com>
2023-09-25 21:00:57 -0400

    Update merge lynx script by improving naming/annotations.

--- scripts/merge_lynx_deployment_registries.py
+++ scripts/merge_lynx_deployment_registries.py
@@ -1,12 +1,15 @@
 from scripts.constants import ARTIFACTS_DIR
 from scripts.registry import merge_registries
 
-lynx_deployment_registry = ARTIFACTS_DIR / "lynx-alpha-13-registry.json"
+lynx_child_deployment_registry = ARTIFACTS_DIR / "lynx" / "lynx-alpha-13-child-registry.json"
+
+# mumbai registry from `nucypher/nucypher`
 lynx_registry_w_subscription_manager = ARTIFACTS_DIR / "contract_registry.json"
+
 output_registry = ARTIFACTS_DIR / "lynx-alpha-13-merged-registry.json"
 
 merge_registries(
-    registry_1_filepath=lynx_deployment_registry,
+    registry_1_filepath=lynx_child_deployment_registry,
     registry_2_filepath=lynx_registry_w_subscription_manager,
     output_filepath=output_registry,
     deprecated_contracts=["StakeInfo"],
