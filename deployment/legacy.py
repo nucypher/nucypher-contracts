d1af24d6ee58f77df178d7b1e67c53261dd435c1
Kieran Prasch <kieranprasch@gmail.com>
2023-10-05 17:57:38 +0200

    utility to convert legacy-style registries to new-style artifacts

---
+++ deployment/legacy.py
@@ -0,0 +1,83 @@
+import os
+from pathlib import Path
+from urllib.parse import urlencode
+
+import requests
+from eth_typing import ChecksumAddress
+from eth_utils import to_checksum_address
+
+from deployment.registry import (
+    ChainId,
+    RegistryEntry,
+    write_registry
+)
+from deployment.utils import _load_json
+
+
+def get_creation_info(api_key: str, chain_id: int, contract_address: ChecksumAddress) -> tuple:
+    networks = {
+        5: ("-goerli", "etherscan.io"),
+        80001: ("-testnet", "polygonscan.com")
+    }
+
+    params = {
+        'module': 'account',
+        'action': 'txlist',
+        'address': contract_address,
+        'page': 1,
+        'sort': 'asc',
+        'apikey': api_key
+    }
+
+    network, explorer = networks[chain_id]
+    base_url = f"https://api{network}.{explorer}/api"
+    url = f"{base_url}?{urlencode(params)}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data['status'] == '1' and data['result']:
+        # If there are transactions, the first one will be the contract creation transaction
+        tx = data['result'][0]
+        tx_hash = tx['hash']
+        block_number = tx['blockNumber']
+        deployer = tx['from']
+    else:
+        raise ValueError(f"Could not find contract creation transaction for {contract_address}")
+
+    return tx_hash, block_number, to_checksum_address(deployer)
+
+
+def convert_legacy_registry(
+        legacy_filepath: Path,
+        output_filepath: Path,
+        chain_id: ChainId,
+) -> None:
+    """Converts a legacy nucypher-style contract registry to a new-style registry."""
+
+    if not legacy_filepath.exists():
+        raise FileNotFoundError(f"Legacy registry not found at {legacy_filepath}")
+    api_key = os.environ.get("ETHERSCAN_API_KEY")
+    if not api_key:
+        raise ValueError("Please set the ETHERSCAN_API_KEY environment variable.")
+
+    legacy_registry_entries = _load_json(filepath=legacy_filepath)
+    new_registry_entries = list()
+    for entry in legacy_registry_entries:
+        name, _version, address, abi = entry[0], entry[1], entry[2], entry[3]
+        tx_hash, block_number, deployer = get_creation_info(
+            api_key=api_key,
+            chain_id=chain_id,
+            contract_address=address
+        )
+        new_registry_entry = RegistryEntry(
+            chain_id=chain_id,
+            name=name,
+            address=address,
+            abi=abi,
+            tx_hash=tx_hash,
+            block_number=block_number,
+            deployer=deployer
+        )
+        new_registry_entries.append(new_registry_entry)
+    write_registry(entries=new_registry_entries, filepath=output_filepath)
+    print(f"Converted legacy registry to {output_filepath}")
