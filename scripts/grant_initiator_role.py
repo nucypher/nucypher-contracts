1a539496693cb25f2aa9da0fc8c25515f4e32817
derekpierre <derek.pierre@gmail.com>
2023-10-10 20:55:56 -0400

    Make grant_initiator_role script a common script and parameterize using click options. Fix bug in Transactor to set account when it is specified.

--- scripts/grant_initiator_role.py
+++ scripts/grant_initiator_role.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
+from pathlib import Path
 
-
+import click
 from ape import networks, project
-from deployment.constants import ARTIFACTS_DIR
+from ape.cli import NetworkBoundCommand, account_option, network_option
 from deployment.params import Transactor
 from deployment.registry import contracts_from_registry
 from deployment.utils import check_plugins
 
-TAPIR_REGISTRY_FILEPATH = ARTIFACTS_DIR / "tapir.json"
 
-
-def main():
+@click.command(cls=NetworkBoundCommand)
+@network_option(required=True)
+@account_option()
+@click.option(
+    "--registry-filepath",
+    "-r",
+    help="Filepath to registry file",
+    type=click.Path(dir_okay=False, exists=True, path_type=Path),
+    required=True,
+)
+def cli(network, account, registry_filepath):
     check_plugins()
-    transactor = Transactor()
+    transactor = Transactor(account)
     deployments = contracts_from_registry(
-        filepath=TAPIR_REGISTRY_FILEPATH, chain_id=networks.active_provider.chain_id
+        filepath=registry_filepath, chain_id=networks.active_provider.chain_id
     )
     coordinator = deployments[project.Coordinator.contract_type.name]
     initiator_role_hash = coordinator.INITIATOR_ROLE()
