5a593b0b2e47951526af3563b3f9d790ca622c35
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-10-30 09:59:00 +0100

    feature(ts): expose contract registry as ts package

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import lynxRegistryJson from "../deployment/artifacts/lynx.json";
+import mainnetRegistryJson from "../deployment/artifacts/mainnet.json";
+import tapirRegistryJson from "../deployment/artifacts/tapir.json";
+
+
+export type Abi = unknown;
+
+export type DeployedContract = {
+  address: string;
+  abi: Abi;
+};
+
+// Only expose contracts that are used in the SDK
+export const contractNames = [
+  "Coordinator",
+  "GlobalAllowList",
+  "SubscriptionManager"
+] as const;
+
+export type ContractName = (typeof contractNames)[number];
+
+export type Contract = {
+  name: ContractName;
+  abi: Abi;
+};
+
+export type ContractRegistry = {
+  [chainId: string]: Record<string, DeployedContract>;
+};
+
+export const domainRegistry: Record<string, ContractRegistry> = {
+  lynx: lynxRegistryJson,
+  tapir: tapirRegistryJson,
+  mainnet: mainnetRegistryJson
+};
+
+export type Domain = "mainnet" | "oryx" | "tapir" | "lynx";
+
+export type ChainId = 1 | 5 | 137 | 80001;
+
+export type ChecksumAddress = `0x${string}`;
+
+export const getContract = (
+  domain: Domain,
+  chainId: ChainId,
+  contract: ContractName
+): ChecksumAddress => {
+  const registry = domainRegistry[domain];
+  if (!registry) {
+    throw new Error(`No contract registry found for domain: ${domain}`);
+  }
+
+  const contracts = registry[chainId as ChainId];
+  if (!contracts) {
+    throw new Error(`No contracts found for chainId: ${chainId}`);
+  }
+
+  const deployedContract = contracts[contract];
+  if (!deployedContract) {
+    throw new Error(`No contract found for name: ${deployedContract}`);
+  }
+
+  return deployedContract.address as ChecksumAddress;
+};
