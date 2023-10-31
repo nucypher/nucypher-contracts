3ecfd840dcaa064fbbcda54e9cc312e259773d83
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-10-31 12:42:47 +0100

    chore(test): test negative cases in ts registry

--- test/registry.spec.ts
+++ test/registry.spec.ts
@@ -19,4 +19,28 @@ describe('registry', () => {
       expect(contractAddress).toBeDefined();
     });
   }
+
+  it('should throw for invalid domain', () => {
+    expect(() => getContract('invalid-domain', 80001, 'Coordinator')).toThrow();
+  });
+
+  it('should throw for invalid chainId', () => {
+    expect(() => getContract('lynx', 0, 'Coordinator')).toThrow();
+  });
+
+  it('should throw for invalid contract', () => {
+    expect(() => getContract('lynx', 80001, 'InvalidContract')).toThrow();
+  });
+
+  it('should return the same contract address for the same domain, chainId, and contract', () => {
+    const contractAddress1 = getContract('lynx', 80001, 'Coordinator');
+    const contractAddress2 = getContract('lynx', 80001, 'Coordinator');
+    expect(contractAddress1).toEqual(contractAddress2);
+  });
+
+  it('should return different contract addresses for different domains', () => {
+    const contractAddress1 = getContract('lynx', 80001, 'Coordinator');
+    const contractAddress2 = getContract('tapir', 80001, 'Coordinator');
+    expect(contractAddress1).not.toEqual(contractAddress2);
+  });
 });
