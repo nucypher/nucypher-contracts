f382472a5d42dc3ea4ece999c7bd3d9b9bb1d752
derekpierre <derek.pierre@gmail.com>
2024-01-15 09:14:59 -0500

    Add script to specifically update Coordinator contract on lynx.

---
+++ scripts/lynx/upgrade_coordinator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from ape import project
+from deployment.constants import CONSTRUCTOR_PARAMS_DIR, ARTIFACTS_DIR
+from deployment.params import Deployer
+from deployment.registry import contracts_from_registry
+
+VERIFY = False
+CONSTRUCTOR_PARAMS_FILEPATH = CONSTRUCTOR_PARAMS_DIR / "lynx" / "upgrade-coordinator.yml"
+
+
+def main():
+    """
+    This script upgrades Coordinator on Lynx/Mumbai.
+    """
+
+    deployer = Deployer.from_yaml(filepath=CONSTRUCTOR_PARAMS_FILEPATH, verify=VERIFY)
+    instances = contracts_from_registry(filepath=ARTIFACTS_DIR/'lynx.json', chain_id=80001)
+
+    coordinator = deployer.upgrade(
+        project.Coordinator,
+        instances[project.Coordinator.contract_type.name].address
+    )
+
+    deployments = [
+        coordinator,
+    ]
+
+    deployer.finalize(deployments=deployments)
