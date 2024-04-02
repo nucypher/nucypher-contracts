6a1f2dc07c69f3161b4864129eebc89c2dacb411
derekpierre <derek.pierre@gmail.com>
2024-04-02 16:43:58 -0400

    Update child deployment comments to reflect amoy deployment.

--- scripts/tapir/deploy_child.py
+++ scripts/tapir/deploy_child.py
@@ -29,6 +29,14 @@ def main():
     ape-polygon      0.6.6
     ape-solidity     0.6.9
     eth-ape          0.6.19
+
+    April 2nd, 2024
+    ape run tapir deploy_child --network polygon:amoy:infura
+    ape-etherscan    0.7.0
+    ape-infura       0.7.2
+    ape-polygon      0.7.2
+    ape-solidity     0.7.1
+    eth-ape          0.7.7
     """
 
     deployer = Deployer.from_yaml(filepath=CONSTRUCTOR_PARAMS_FILEPATH, verify=VERIFY)
