76e57daed86680b7e773aede70b16e25ba85e49e
Piotr Roslaniec <p.roslaniec@gmail.com>
2024-04-18 13:24:38 +0200

    wip: add way too much comments

--- contracts/contracts/coordination/GlobalAllowList.sol
+++ contracts/contracts/coordination/GlobalAllowList.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./IEncryptionAuthorizer.sol";
 import "./Coordinator.sol";
 
+/**
+ * @title GlobalAllowList
+ * @notice Manages a global allow list of addresses that are authorized to decrypt ciphertexts.
+ */
 contract GlobalAllowList is IEncryptionAuthorizer {
     using MessageHashUtils for bytes32;
     using ECDSA for bytes32;
@@ -17,18 +21,33 @@ contract GlobalAllowList is IEncryptionAuthorizer {
 
     mapping(uint32 => uint256) public authActions;
 
+    /**
+     * @notice Emitted when an address authorization is set
+     * @param ritualId The ID of the ritual
+     * @param _address The address that is authorized
+     * @param isAuthorized The authorization status
+     */
     event AddressAuthorizationSet(
         uint32 indexed ritualId,
         address indexed _address,
         bool isAuthorized
     );
 
+    /**
+     * @notice Sets the coordinator contract
+     * @dev The coordinator contract cannot be a zero address and must have a valid number of rituals
+     * @param _coordinator The address of the coordinator contract
+     */
     constructor(Coordinator _coordinator) {
         require(address(_coordinator) != address(0), "Coordinator cannot be zero address");
         require(_coordinator.numberOfRituals() >= 0, "Invalid coordinator");
         coordinator = _coordinator;
     }
 
+    /**
+     * @notice Checks if the sender is the authority of the ritual
+     * @param ritualId The ID of the ritual
+     */
     modifier canSetAuthorizations(uint32 ritualId) virtual {
         require(
             coordinator.getAuthority(ritualId) == msg.sender,
@@ -37,20 +56,46 @@ contract GlobalAllowList is IEncryptionAuthorizer {
         _;
     }
 
+    /**
+     * @notice Returns the key used to lookup authorizations
+     * @param ritualId The ID of the ritual
+     * @param encryptor The address of the encryptor
+     * @return The key used to lookup authorizations
+     */
     function lookupKey(uint32 ritualId, address encryptor) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(ritualId, encryptor));
     }
 
+    /**
+     * @notice Checks if an address is authorized for a ritual
+     * @param ritualId The ID of the ritual
+     * @param encryptor The address of the encryptor
+     * @return The authorization status
+     */
     function isAddressAuthorized(uint32 ritualId, address encryptor) public view returns (bool) {
         return authorizations[lookupKey(ritualId, encryptor)];
     }
 
+    /**
+     * @notice Checks if an address is authorized for a ritual
+     * @dev This function is called before the isAuthorized function
+     * @param ritualId The ID of the ritual
+     * @param evidence The evidence provided
+     * @param ciphertextHeader The header of the ciphertext
+     */
     function _beforeIsAuthorized(
         uint32 ritualId,
         bytes memory evidence,
         bytes memory ciphertextHeader
     ) internal view virtual {}
 
+    /**
+     * @notice Checks if an address is authorized for a ritual
+     * @param ritualId The ID of the ritual
+     * @param evidence The evidence provided
+     * @param ciphertextHeader The header of the ciphertext
+     * @return The authorization status
+     */
     function isAuthorized(
         uint32 ritualId,
         bytes memory evidence,
@@ -63,12 +108,24 @@ contract GlobalAllowList is IEncryptionAuthorizer {
         return isAddressAuthorized(ritualId, recoveredAddress);
     }
 
+    /**
+     * @notice Checks if an address is authorized for a ritual
+     * @dev This function is called before the setAuthorizations function
+     * @param ritualId The ID of the ritual
+     * @param addresses The addresses to be authorized
+     * @param value The authorization status
+     */
     function _beforeSetAuthorization(
         uint32 ritualId,
         address[] calldata addresses,
         bool value
     ) internal view virtual {}
 
+    /**
+     * @notice Authorizes a list of addresses for a ritual
+     * @param ritualId The ID of the ritual
+     * @param addresses The addresses to be authorized
+     */
     function authorize(
         uint32 ritualId,
         address[] calldata addresses
@@ -76,6 +133,11 @@ contract GlobalAllowList is IEncryptionAuthorizer {
         setAuthorizations(ritualId, addresses, true);
     }
 
+    /**
+     * @notice Deauthorizes a list of addresses for a ritual
+     * @param ritualId The ID of the ritual
+     * @param addresses The addresses to be deauthorized
+     */
     function deauthorize(
         uint32 ritualId,
         address[] calldata addresses
@@ -83,6 +145,13 @@ contract GlobalAllowList is IEncryptionAuthorizer {
         setAuthorizations(ritualId, addresses, false);
     }
 
+    /**
+     * @notice Sets the authorization status for a list of addresses for a ritual
+     * @dev Only active rituals can set authorizations
+     * @param ritualId The ID of the ritual
+     * @param addresses The addresses to be authorized or deauthorized
+     * @param value The authorization status
+     */
     function setAuthorizations(uint32 ritualId, address[] calldata addresses, bool value) internal {
         require(coordinator.isRitualActive(ritualId), "Only active rituals can set authorizations");
 
--- contracts/contracts/coordination/Subscription.sol
+++ contracts/contracts/coordination/Subscription.sol
@@ -6,8 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Coordinator.sol";
 
-abstract contract Subscription {
+using SafeERC20 for IERC20;
 
+/**
+ * @title Subscription
+ * @notice Manages the subscription information for rituals.
+ * @dev This contract is abstract and should be extended by a concrete implementation.
+ * It maintains a reference to a Coordinator contract and a fee token (ERC20).
+ * Each subscription has an associated SubscriptionInfo struct which keeps track of the subscription details.
+ */
+abstract contract Subscription {
     struct SubscriptionInfo {
         uint256 paidFor;
         uint256 spent;
@@ -18,33 +26,80 @@ abstract contract Subscription {
     Coordinator coordinator;
     IERC20 feeToken;
 
+    // Mapping from subscription ID to subscription info
     mapping(uint32 => SubscriptionInfo) public subscriptions;
+
+    // Mapping from (ritualId, address) to subscription ID
+    mapping(bytes32 => uint32) public subscribers;
+
     uint32 public numberOfSubscriptions;
 
-    constructor(Coordinator _coordinator, IERC20 _feeToken){
-        // TODO: coordintaor and token checks
+    // TODO: DAO Treasury
+    address public beneficiary;
+
+    /**
+     * @notice Sets the coordinator and fee token contracts
+     * @dev The coordinator and fee token contracts cannot be zero addresses
+     * @param _coordinator The address of the coordinator contract
+     * @param _feeToken The address of the fee token contract
+     * @param _beneficiary The address of the beneficiary
+     */
+    constructor(Coordinator _coordinator, IERC20 _feeToken, address _beneficiary) {
+        require(address(_coordinator) != address(0), "Coordinator cannot be the zero address");
+        require(address(_feeToken) != address(0), "Fee token cannot be the zero address");
+        require(_beneficiary != address(0), "Beneficiary cannot be the zero address");
         coordinator = _coordinator;
         feeToken = _feeToken;
+        beneficiary = _beneficiary;
     }
 
-    function subscriptionFee() pure public returns(uint256) {
-        return 42 * 10**20;  // TODO
+    /**
+     * @notice Returns the subscription fee
+     * @return The subscription fee
+     */
+    function subscriptionFee() public pure returns (uint256) {
+        return 42 * 10 ** 20; // TODO
     }
 
-    function baseExpiration() pure public returns(uint256) {
-        return 52 weeks;  // TODO
+    /**
+     * @notice Returns the base expiration duration
+     * @return The base expiration duration
+     */
+    function baseExpiration() public pure returns (uint256) {
+        return 52 weeks; // TODO
     }
 
-    function newSubscription() external returns(uint256){
+    /**
+     * @notice Returns the key used to lookup authorizations
+     * @param ritualId The ID of the ritual
+     * @param encryptor The address of the encryptor
+     * @return The key used to lookup authorizations
+     */
+    function lookupKey(uint32 ritualId, address encryptor) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(ritualId, encryptor));
+    }
 
+    /**
+     * @notice Creates a new subscription
+     * @param ritualId The ID of the ritual
+     * @return The ID of the new subscription
+     */
+    function newSubscription(uint32 ritualId) external returns (uint256) {
         uint32 subscriptionId = numberOfSubscriptions;
         SubscriptionInfo storage sub = subscriptions[subscriptionId];
         sub.subscriber = msg.sender;
         paySubscriptionFor(subscriptionId);
 
+        subscribers[lookupKey(ritualId, msg.sender)] = subscriptionId;
+
         numberOfSubscriptions += 1;
+        return subscriptionId;
     }
 
+    /**
+     * @notice Pays for a subscription
+     * @param subscriptionId The ID of the subscription
+     */
     function paySubscriptionFor(uint32 subscriptionId) public virtual {
         uint256 amount = subscriptionFee();
 
@@ -55,36 +110,102 @@ abstract contract Subscription {
         feeToken.safeTransferFrom(msg.sender, address(this), amount);
     }
 
+    /**
+     * @notice Checks if a spender can spend from a subscription
+     * @param subscriptionId The ID of the subscription
+     * @param spender The address of the spender
+     * @return True if the spender can spend from the subscription, false otherwise
+     */
     function canSpendFromSubscription(
         uint32 subscriptionId,
         address spender
-    ) public returns(bool){
+    ) public returns (bool) {
         // By default, only coordinator can spend from subscription
         return spender == address(coordinator);
     }
 
+    /**
+     * @notice Spends from a subscription
+     * @param subscriptionId The ID of the subscription
+     * @param amount The amount to spend
+     */
     function spendFromSubscription(uint32 subscriptionId, uint256 amount) external {
         require(canSpendFromSubscription(subscriptionId, msg.sender));
         feeToken.safeTransferFrom(address(this), msg.sender, amount);
     }
 
     // TODO: Withdraw methods for DAO Treasury, cancel subscription, etc
-}
 
-// An upfront subscription for a cohort with a predefined duration and a max number of encryptors
-contract UpfrontSubscriptionWithEncryptorsCap is Subscription {
-
-    uint256 constant DEFAULT_CAP = 1000;
-
-    mapping(uint32 => uint256) authorizationActionCaps;
-
-    constructor(Coordinator _coordinator, IERC20 _feeToken)
-        Subscription(_coordinator, _feeToken){
-
+    /**
+     * @notice Withdraws the contract balance to the beneficiary
+     * @param amount The amount to withdraw
+     */
+    function withdrawToBeneficiary(uint256 amount) external {
+        require(msg.sender == beneficiary, "Only the beneficiary can withdraw");
+        uint256 contractBalance = feeToken.balanceOf(address(this));
+        require(contractBalance >= amount, "Insufficient contract balance");
+        feeToken.safeTransfer(beneficiary, amount);
     }
 
+    /**
+     * @notice Cancels a subscription
+     * @param ritualId The ID of the ritual
+     * @param subscriptionId The ID of the subscription
+     */
+    function cancelSubscription(uint32 ritualId, uint32 subscriptionId) external {
+        require(
+            msg.sender == subscriptions[subscriptionId].subscriber,
+            "Only the subscriber can cancel the subscription"
+        );
+        uint256 refundAmount = subscriptions[subscriptionId].paidFor;
+        feeToken.safeTransfer(msg.sender, refundAmount);
+        delete subscriptions[subscriptionId];
+        delete subscribers[lookupKey(ritualId, msg.sender)];
+    }
+}
+
+/**
+ * @title UpfrontSubscriptionWithEncryptorsCap
+ * @notice Manages upfront subscriptions with a cap on the number of encryptors.
+ * @dev This contract extends the Subscription contract and introduces a cap on the number of encryptors.
+ */
+contract UpfrontSubscriptionWithEncryptorsCap is Subscription {
+    uint256 constant DEFAULT_CAP = 1000;
+
+    // Mapping from subscription ID to the number of authorization actions
+    mapping(uint32 => uint256) authorizationActionCaps;
+
+    /**
+     * @notice Sets the coordinator and fee token contracts
+     * @dev The coordinator and fee token contracts cannot be zero addresses
+     * @param _coordinator The address of the coordinator contract
+     * @param _feeToken The address of the fee token contract
+     */
+    constructor(
+        Coordinator _coordinator,
+        IERC20 _feeToken,
+        address _beneficiary
+    ) Subscription(_coordinator, _feeToken, _beneficiary) {}
+
+    /**
+     * @notice Pays for a subscription and increases the authorization actions cap
+     * @param subscriptionId The ID of the subscription
+     */
     function paySubscriptionFor(uint32 subscriptionId) public virtual override {
         super.paySubscriptionFor(subscriptionId);
         authorizationActionCaps[subscriptionId] += DEFAULT_CAP;
+    }
+
+    /**
+     * @notice Returns the authorization actions cap for a given ritual and spender
+     * @param ritualId The ID of the ritual
+     * @param spender The address of the spender
+     * @return The authorization actions cap
+     */
+    function authorizationActionsCap(
+        uint32 ritualId,
+        address spender
+    ) public view returns (uint256) {
+        return authorizationActionCaps[subscribers[lookupKey(ritualId, spender)]];
     }
 }
