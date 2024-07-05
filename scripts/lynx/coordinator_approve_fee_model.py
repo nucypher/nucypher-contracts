5b57c0acf102d1f1f4b40e18e48813a4df486897
Manuel Montenegro <manuel@nucypher.com>
2024-07-05 16:47:51 +0200

    Add coordinator approve fee model script

---
+++ scripts/lynx/coordinator_approve_fee_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from ape import project, networks
+
+from deployment.constants import ARTIFACTS_DIR
+from deployment.params import Transactor
+from deployment.registry import contracts_from_registry
+
+LYNX_REGISTRY_FILEPATH = ARTIFACTS_DIR / "lynx.json"
+
+def main():
+    """
+    Coordinator approves the fee model for BqETHSubscription
+
+    ape run lynx coordinator_approve_fee_model --network polygon:amoy:infura
+    """
+
+    transactor = Transactor()
+    deployments = contracts_from_registry(
+        filepath=LYNX_REGISTRY_FILEPATH, chain_id=networks.active_provider.chain_id
+    )
+    coordinator = deployments[project.Coordinator.contract_type.name]
+    bqeth_subscription = deployments[project.BqETHSubscription.contract_type.name]
+
+    # Grant TREASURY_ROLE
+    TREASURY_ROLE = coordinator.TREASURY_ROLE()
+    transactor.transact(
+        coordinator.grantRole,
+        TREASURY_ROLE,
+        transactor.get_account().address
+    )
+    transactor.transact(coordinator.approveFeeModel, bqeth_subscription.address)
