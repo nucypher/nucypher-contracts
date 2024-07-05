b391ff3481b6a48c3167176702339596d75a0635
James Campbell <james.campbell@tanti.org.uk>
2024-07-05 15:51:58 +0200

    Use dotenv for sensitive variables

--- scripts/bqeth_subscription.py
+++ scripts/bqeth_subscription.py
@@ -2,48 +2,50 @@ import json
 import os
 
 import requests
+from dotenv import load_dotenv
 from eth_account.messages import encode_defunct
 from web3 import Web3
+
+"""
+Read from a .env file
+"""
+PROVIDER_URL = os.environ.get("PROVIDER_URL")
+PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
+PUBLIC_ADDRESS = os.environ.get("PUBLIC_ADDRESS")
 
 """Constants"""
 BASE_FEE_RATE = 42
 MAX_NODES = 10
 ENCRYPTORS_FEE_RATE = 77
 PACKAGE_DURATION = 10 * 24 * 60 * 60  # 10 days
-YELLOW_PERIOD = 24 * 60 * 60  # 1 day
-RED_PERIOD = 5 * 24 * 60 * 60  # 5 days
 
-TESTNET_URI = "https://goerli.infura.io/v3/YOUR_PROJECT_ID"
-NUCYPHER_DOMAIN = "tapir"
+
+NUCYPHER_DOMAIN = "lynx"
 CHAIN = "80002"
-PORTER_ENDPOINT = "https://porter-tapir.nucypher.io"
-NUM_NODES = 3
+PORTER_ENDPOINT = "https://lynx-tapir.nucypher.io"
+NUM_NODES = 2
 
 with open("deployment/artifacts/tapir.json", "r") as f:
     registry = json.load(f)
 
-# SUBSCRIPTION_CONTRACT_ADDRESS = "0x..."
-# SUBSCRIPTION_CONTRACT_ABI = [...]
-ERC20_CONTRACT_ADDRESS = registry[CHAIN]["TapirRitualToken"]["address"]
-ERC20_CONTRACT_ABI = registry[CHAIN]["TapirRitualToken"]["abi"]
+SUBSCRIPTION_CONTRACT_ADDRESS = registry[CHAIN]["BqETHSubscription"]["address"]
+SUBSCRIPTION_CONTRACT_ABI = registry[CHAIN]["BqETHSubscription"]["abi"]
+ERC20_CONTRACT_ADDRESS = registry[CHAIN][NUCYPHER_DOMAIN.title() + "RitualToken"]["address"]
+ERC20_CONTRACT_ABI = registry[CHAIN][NUCYPHER_DOMAIN.title() + "RitualToken"]["abi"]
 COORDINATOR_CONTRACT_ADDRESS = registry[CHAIN]["Coordinator"]["address"]
 COORDINATOR_CONTRACT_ABI = registry[CHAIN]["Coordinator"]["abi"]
 GLOBAL_ALLOW_LIST_CONTRACT_ADDRESS = registry[CHAIN]["GlobalAllowList"]["address"]
 GLOBAL_ALLOW_LIST_CONTRACT_ABI = registry[CHAIN]["GlobalAllowList"]["abi"]
 
-"""
-Replace with your account details
-"""
-ADOPTER_PRIVATE_KEY = "0x..."
-ADOPTER_ADDRESS = "0x..."
-
 
 """Connect to the testnet"""
-w3 = Web3(Web3.HTTPProvider(TESTNET_URI))
+w3 = Web3(Web3.HTTPProvider(PROVIDER_URL))
 
 """Set up contract instances"""
 erc20_contract = w3.eth.contract(address=ERC20_CONTRACT_ADDRESS, abi=ERC20_CONTRACT_ABI)
-# subscription_contract = w3.eth.contract(address=SUBSCRIPTION_CONTRACT_ADDRESS, abi=SUBSCRIPTION_CONTRACT_ABI)
+subscription_contract = w3.eth.contract(
+    address=SUBSCRIPTION_CONTRACT_ADDRESS, abi=SUBSCRIPTION_CONTRACT_ABI
+)
 coordinator_contract = w3.eth.contract(
     address=COORDINATOR_CONTRACT_ADDRESS, abi=COORDINATOR_CONTRACT_ABI
 )
@@ -51,19 +53,27 @@ global_allow_list_contract = w3.eth.contract(
     address=GLOBAL_ALLOW_LIST_CONTRACT_ADDRESS, abi=GLOBAL_ALLOW_LIST_CONTRACT_ABI
 )
 
+YELLOW_PERIOD = subscription_contract.functions.yellowPeriodDuration().call()
+RED_PERIOD = subscription_contract.functions.redPeriodDuration().call()
+
+
 """Approve ERC20 token for subscription contract"""
-# erc20_contract.functions.approve(SUBSCRIPTION_CONTRACT_ADDRESS, 10 * BASE_FEE_RATE * PACKAGE_DURATION * MAX_NODES).transact({'from': ADOPTER_ADDRESS})
+erc20_contract.functions.approve(
+    SUBSCRIPTION_CONTRACT_ADDRESS, 10 * BASE_FEE_RATE * PACKAGE_DURATION * MAX_NODES
+).transact({"from": ADOPTER_ADDRESS})
 
 """Pay for a new subscription period"""
-# tx_hash = subscription_contract.functions.payForSubscription(0).transact({'from': ADOPTER_ADDRESS})
-# tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-# print(f"Paid for a new subscription period. Transaction receipt: {tx_receipt}")
+tx_hash = subscription_contract.functions.payForSubscription(0).transact({"from": PUBLIC_ADDRESS})
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Paid for a new subscription period. Transaction receipt: {tx_receipt}")
 
 """Pay for encryptor slots"""
 encryptor_slots = 5
-# tx_hash = subscription_contract.functions.payForEncryptorSlots(encryptor_slots).transact({"from": ADOPTER_ADDRESS})
-# tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-# print(f"Paid for {encryptor_slots} encryptor slots. Transaction receipt: {tx_receipt}")
+tx_hash = subscription_contract.functions.payForEncryptorSlots(encryptor_slots).transact(
+    {"from": PUBLIC_ADDRESS}
+)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Paid for {encryptor_slots} encryptor slots. Transaction receipt: {tx_receipt}")
 
 """Initiate a ritual"""
 url = f"{PORTER_ENDPOINT}/get_ursulas?quantity={NUM_NODES}"
@@ -75,15 +85,14 @@ if response.status_code == 200:
     providers = [u["checksum_address"] for u in data["result"]["ursulas"]]
 else:
     print(f"Error: {response.status_code} - {response.text}")
-# ritual_id = coordinator_contract.functions.initiateRitual(
-#     # Replace with the appropriate fee model, providers, authority, duration, and access controller
-#     fee_model=SUBSCRIPTION_CONTRACT_ADDRESS,
-#     providers=providers,
-#     authority=ADOPTER_ADDRESS,
-#     duration=PACKAGE_DURATION, # yellow period, red period, _subscriptionPeriodDuration
-#     access_controller=GLOBAL_ALLOW_LIST_CONTRACT_ADDRESS
-# ).transact({'from': ADOPTER_ADDRESS})
-# print(f"Initiated ritual with ID: {ritual_id}")
+ritual_id = coordinator_contract.functions.initiateRitual(
+    fee_model=SUBSCRIPTION_CONTRACT_ADDRESS,
+    providers=providers,
+    authority=PUBLIC_ADDRESS,
+    duration=PACKAGE_DURATION,  # yellow period, red period, _subscriptionPeriodDuration
+    access_controller=GLOBAL_ALLOW_LIST_CONTRACT_ADDRESS,
+).transact({"from": PUBLIC_ADDRESS})
+print(f"Initiated ritual with ID: {ritual_id}")
 
 
 # # Test operations during Yellow period
