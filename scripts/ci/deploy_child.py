d315687ece480b44afea3ee2fb1f0ffd177aa00b
derekpierre <derek.pierre@gmail.com>
2025-04-10 10:24:48 -0400

    Improve our CI sanity test to also ensure that contract upgrades work appropriately.

--- scripts/ci/deploy_child.py
+++ scripts/ci/deploy_child.py
@@ -1,11 +1,43 @@
 #!/usr/bin/python3
+import filecmp
+from pathlib import Path
+
 from ape import accounts, networks, project
 
-from deployment.constants import CONSTRUCTOR_PARAMS_DIR
+from deployment.constants import ARTIFACTS_DIR, CONSTRUCTOR_PARAMS_DIR
 from deployment.params import Deployer
+from deployment.registry import ConflictResolution, merge_registries
 
 VERIFY = False
 CONSTRUCTOR_PARAMS_FILEPATH = CONSTRUCTOR_PARAMS_DIR / "ci" / "child.yml"
+UPGRADE_PARAMS_FILEPATH = CONSTRUCTOR_PARAMS_DIR / "ci" / "upgrade-coordinator.yml"
+
+UPGRADED_COORDINATOR_FILE_NAME = "ci-upgrade-coordinator.json"
+
+ORIGINAL_DEPLOYMENT_ARTIFACT = ARTIFACTS_DIR / "ci.json"
+UPGRADED_DEPLOYMENT_ARTIFACT = ARTIFACTS_DIR / UPGRADED_COORDINATOR_FILE_NAME
+FINAL_DEPLOYMENT_ARTIFACT = ARTIFACTS_DIR / "ci-final.json"
+
+
+def create_upgrade_coordinator_yaml(output_file: Path, taco_child_application_address: str) -> None:
+    """
+    Creates a YAML file for the upgrade process.
+    """
+    yaml_text = f"""deployment:
+  name: ci-upgrade-coordinator
+  chain_id: 80002
+
+artifacts:
+  dir: ./deployment/artifacts/
+  filename: {UPGRADED_COORDINATOR_FILE_NAME}
+
+contracts:
+  - Coordinator:
+      constructor:
+        _application: "{taco_child_application_address}"
+    """
+    with open(output_file, "w") as file:
+        file.write(yaml_text)
 
 
 def main():
@@ -41,5 +73,39 @@ def main():
 
     deployer.finalize(deployments=deployments)
 
-    # remove registry file now that task is complete
+    # dynamically create the YAML file for the upgrade of the Coordinator contract
+    create_upgrade_coordinator_yaml(UPGRADE_PARAMS_FILEPATH, str(taco_child_application.address))
+
+    with networks.ethereum.local.use_provider("test"):
+        test_account = accounts.test_accounts[0]
+        upgrade_deployer = Deployer.from_yaml(
+            filepath=UPGRADE_PARAMS_FILEPATH,
+            verify=VERIFY,
+            account=test_account,
+            non_interactive=True,
+        )
+
+        upgraded_coordinator = upgrade_deployer.upgrade(project.Coordinator, coordinator.address)
+
+    upgraded_deployments = [
+        upgraded_coordinator,
+    ]
+
+    upgrade_deployer.finalize(deployments=upgraded_deployments)
+
+    # merge registries
+    merge_registries(
+        registry_1_filepath=deployer.registry_filepath,
+        registry_2_filepath=upgrade_deployer.registry_filepath,
+        output_filepath=FINAL_DEPLOYMENT_ARTIFACT,
+        force_conflict_resolution=ConflictResolution.USE_2,
+    )
+
+    # diff
+    assert filecmp.cmp(ORIGINAL_DEPLOYMENT_ARTIFACT, FINAL_DEPLOYMENT_ARTIFACT, shallow=False)
+
+    # remove created files
     deployer.registry_filepath.unlink()
+    upgrade_deployer.registry_filepath.unlink()
+    FINAL_DEPLOYMENT_ARTIFACT.unlink()
+    UPGRADE_PARAMS_FILEPATH.unlink()
