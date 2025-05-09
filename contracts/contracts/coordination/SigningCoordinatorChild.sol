1a28ec8e884deba4068d209898eef1bb581940df
derekpierre <derek.pierre@gmail.com>
2025-05-09 18:57:17 -0400

    Initial musing about canonical SigningCoordinator contract that can be on Ethereum with SigningCoordinatorChild contracts on L2s.

---
+++ contracts/contracts/coordination/SigningCoordinatorChild.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "./ISigningCoordinatorChild.sol";
+import "./IThresholdSigningMultisig.sol";
+import "./ThresholdSigningMultisigCloneFactory.sol";
+
+contract SigningCoordinatorChild is ISigningCoordinatorChild {
+    mapping(uint32 => address) public cohortMultisigs;
+    ThresholdSigningMultisigCloneFactory public immutable signingMultisigFactory;
+
+    constructor(ThresholdSigningMultisigCloneFactory _signingMultisigFactory) {
+        signingMultisigFactory = _signingMultisigFactory;
+    }
+
+    function deployCohortMultiSig(
+        uint32 cohortId,
+        address[] calldata signers,
+        uint16 threshold
+    ) external {
+        require(cohortMultisigs[cohortId] == address(0), "Multisig already deployed");
+        address multisig = signingMultisigFactory.deploySigningMultisig(
+            signers,
+            threshold,
+            address(this),
+            cohortId
+        );
+        cohortMultisigs[cohortId] = multisig;
+        emit CohortMultisigDeployed(cohortId, multisig);
+    }
+
+    function updateMultiSigParameters(
+        uint32 cohortId,
+        address[] calldata signers,
+        uint16 threshold,
+        bool clearSigners
+    ) external {
+        address multisig = cohortMultisigs[cohortId];
+        require(multisig != address(0), "Multisig not deployed");
+        IThresholdSigningMultisig multisigContract = IThresholdSigningMultisig(multisig);
+        multisigContract.updateMultiSigParameters(signers, threshold, clearSigners);
+        emit CohortMultisigUpdated(cohortId, multisig, signers, threshold, clearSigners);
+    }
+}
