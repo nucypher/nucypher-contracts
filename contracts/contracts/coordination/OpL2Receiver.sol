63dbe87894141492ebab6c6c352520261de3cb8a
derekpierre <derek.pierre@gmail.com>
2025-05-13 20:32:46 -0400

    Update implementations of sender/receiver interfaces to match updated interfaces.

--- contracts/contracts/coordination/OpL2Receiver.sol
+++ contracts/contracts/coordination/OpL2Receiver.sol
@@ -2,26 +2,35 @@
 
 pragma solidity ^0.8.0;
 
+import "./IL2Receiver.sol";
+
 interface ICrossDomainMessenger {
     function xDomainMessageSender() external view returns (address);
 }
 
-contract L2Receiver {
+/**
+ * @title OpL2Receiver
+ * @notice Contract is used to receive data from the L1 sender contract via the bridge messenger.
+ */
+contract OpL2Receiver is IL2Receiver {
     address public messenger;
     address public allowedSender;
 
     event Executed(address target, bytes result);
 
-    constructor(address _messenger) {
+    /**
+     * @param _messenger The address of the CrossDomainMessenger contract.
+     */
+    constructor(address _messenger, address _allowedSender) {
         messenger = _messenger;
-    }
-
-    function setAllowedSender(address _allowedSender) external {
-        require(allowedSender == address(0), "Allowed sender already set");
         allowedSender = _allowedSender;
     }
 
-    function execute(bytes calldata data) external {
+    /**
+     * @notice Receives data from the L1 sender contract via the messenger.
+     * @param data The data sent from the L1 sender contract.
+     */
+    function recvData(bytes calldata data) external {
         require(msg.sender == messenger, "Not from messenger");
         require(
             ICrossDomainMessenger(messenger).xDomainMessageSender() == allowedSender,
