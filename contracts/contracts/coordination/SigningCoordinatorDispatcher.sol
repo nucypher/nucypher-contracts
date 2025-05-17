cac7d8c7f21248f6e4ee681b0c5c488e1f05748e
derekpierre <derek.pierre@gmail.com>
2025-05-17 07:43:32 -0400

    Update contracts to follow parent-child dynamic of parent being deployed first then child taking parent as constructor arg, and then parent initializing with child arg. Ensure callers along the dispatch chain validate callers of functions. Update tests accordingly.

--- contracts/contracts/coordination/SigningCoordinatorDispatcher.sol
+++ contracts/contracts/coordination/SigningCoordinatorDispatcher.sol
@@ -2,25 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
-import "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ISigningCoordinatorChild.sol";
 import "./IL1Sender.sol";
 
-contract SigningCoordinatorDispatcher is Initializable, OwnableUpgradeable {
+contract SigningCoordinatorDispatcher is Ownable {
     struct DispatchTarget {
         address l1Sender;
         address signingCoordinatorChild;
     }
 
+    address public immutable signingCoordinator;
     mapping(uint256 => DispatchTarget) public dispatchMap;
 
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() public initializer {
-        __Ownable_init(msg.sender);
+    constructor(address _signingCoordinator) Ownable(msg.sender) {
+        require(_signingCoordinator.code.length > 0, "Signing app must be contract");
+        signingCoordinator = _signingCoordinator;
     }
 
     function register(
@@ -28,7 +25,7 @@ contract SigningCoordinatorDispatcher is Initializable, OwnableUpgradeable {
         address l1Sender,
         address signingCoordinatorChild
     ) external onlyOwner {
-        require(chainId != 0, "Invalid chain ID");
+        require(chainId > 0, "Invalid chain ID");
         if (chainId != block.chainid) {
             require(l1Sender != address(0), "Invalid L1 sender");
         }
@@ -36,7 +33,13 @@ contract SigningCoordinatorDispatcher is Initializable, OwnableUpgradeable {
         dispatchMap[chainId] = DispatchTarget(l1Sender, signingCoordinatorChild);
     }
 
+    function unregister(uint256 chainId) external onlyOwner {
+        require(chainId != 0, "Invalid chain ID");
+        delete dispatchMap[chainId];
+    }
+
     function dispatch(uint256 chainId, bytes calldata callData) external {
+        require(signingCoordinator == msg.sender, "Unauthorized caller");
         DispatchTarget memory target = dispatchMap[chainId];
         require(target.signingCoordinatorChild != address(0), "Unknown target");
         if (chainId == block.chainid) {
