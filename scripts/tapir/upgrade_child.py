a231c7d63b35f06f32bdfce49e99ecf84e3ff139
derekpierre <derek.pierre@gmail.com>
2025-08-12 21:12:23 -0400

    Update upgrade script for tapir child contracts.

--- scripts/tapir/upgrade_child.py
+++ scripts/tapir/upgrade_child.py
@@ -12,23 +12,28 @@ CONSTRUCTOR_PARAMS_FILEPATH = CONSTRUCTOR_PARAMS_DIR / "tapir" / "upgrade-child.
 
 def main():
     """
-    This script upgrades TACoChildApplication and Coordinator on Tapir/Amoy.
+    This script upgrades child contracts for Tapir on Polygon Amoy.
     """
 
     deployer = Deployer.from_yaml(filepath=CONSTRUCTOR_PARAMS_FILEPATH, verify=VERIFY)
     instances = contracts_from_registry(filepath=ARTIFACTS_DIR / "tapir.json", chain_id=80002)
 
-    child_application = deployer.upgrade(
+    mock_polygon_child = deployer.deploy(project.MockPolygonChild)
+
+    taco_child_application = deployer.upgrade(
         project.TACoChildApplication,
         instances[project.TACoChildApplication.contract_type.name].address,
     )
+
+    deployer.transact(mock_polygon_child.setChildApplication, taco_child_application.address)
 
     coordinator = deployer.upgrade(
         project.Coordinator, instances[project.Coordinator.contract_type.name].address
     )
 
     deployments = [
-        child_application,
+        mock_polygon_child,
+        taco_child_application,
         coordinator,
     ]
 
