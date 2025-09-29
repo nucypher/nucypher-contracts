b14d11cbafad10e74e3a76b5b3a6c45625a958ee
David Núñez <david@nucypher.com>
2025-09-29 19:27:15 +0200

    Remove useless test

--- tests/test_example.py
+++
@@ -1,9 +0,0 @@
-
-
-# Just a dummy test to give the system something to run
-def test_example(project, accounts):
-    # Before execution, the contract needs to be deployed
-    project.ReEncryptionValidator.deploy(sender=accounts[0])
-
-    # Then it can be called as:
-    # ReEncryptionValidator[0].validateCFrag(capsule_bytes, cfrag_bytes, precomputed_data)
